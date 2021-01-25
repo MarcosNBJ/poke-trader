1c143c3e03fb611fba4c9601810b1c4fc1a2adfa
MarcosNBJ <marcosnery.comp@gmail.com>
2021-01-25 20:28:03 -0300

    feat: adding more fields to pokemon model

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -3,12 +3,19 @@
 class Pokemon
   include Mongoid::Document
   include Mongoid::Timestamps
+  
   field :name, type: String
   field :base_experience, type: Integer
+  field :hp, type: Integer
+  field :attack, type: Integer
+  field :defense, type: Integer
   field :sprite, type: String
 
   validates :name,
             :base_experience,
+            :hp,
+            :attack,
+            :defense,
             :sprite, presence: true
 
   validates :base_experience, numericality: { greater_than: 0, only_integer: true }
