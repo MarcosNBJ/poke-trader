f20f9b461b396f5c46aff341eaae97ac5518b8b4
MarcosNBJ <marcosnery.comp@gmail.com>
2021-01-26 12:53:11 -0300

    refact: splitting components

---
+++ app/javascript/components/PokemonsContainer.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import InfiniteScroll from 'react-infinite-scroller';
+
+function PokemonsContainer({ loadMorePokemons, pokemons, addTrainerPokemons }) {
+    return (
+        <div className="sprites_row">
+
+            <div className="infinite-scroll">
+
+                <InfiniteScroll
+                    pageStart={0}
+                    loadMore={loadMorePokemons}
+                    hasMore={false}
+                    loader={<div className="loader" key={0}>Loading ...</div>}
+                >
+                    <div className="pokemon-list">
+                        {pokemons.map((pokemon, index) => (
+                            <div className="pokemon" key={index}>
+                                <div className="card" >
+                                    <img className="card-img-top" src={pokemon.sprite} alt="Card image cap" />
+                                    <div className="card-body">
+                                        <h6 className="card-text">{pokemon.name}</h6>
+                                        <button className="btn btn-primary m-1"
+                                            onClick={() => addTrainerPokemons(pokemon, "trainerOne")}>T1</button>
+                                        <button className="btn btn-secondary m-1"
+                                            onClick={() => addTrainerPokemons(pokemon, "trainerTwo")}
+                                        >T2</button>
+
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </InfiniteScroll>
+            </div>
+
+        </div>
+    )
+}
+
+export default PokemonsContainer
--- app/javascript/components/Trade.js
+++ app/javascript/components/Trade.js
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from "react";
-import InfiniteScroll from 'react-infinite-scroller';
 import backendApi from "./backendApi"
-import Message from './Message'; 
+import Message from './Message';
+import TrainerContainer from './TrainerContainer';
+import PokemonsContainer from './PokemonsContainer';
+
 
 function Trade() {
 
   const [trainerOne, settrainerOne] = useState([]);
   const [trainerTwo, settrainerTwo] = useState([]);
-  const [hasMore, setHasMore] = useState(false);
   const [offset, setOffset] = useState(0);
   const [isFair, setIsFair] = useState(false);
   const [missingXp, setMissingXp] = useState(0);
   const [pokemons, setPokemons] = useState([]);
+  const [resultMessage, setResultMessage] = useState("");
 
 
   const loadMorePokemons = () => {
@@ -26,24 +28,30 @@ function Trade() {
     })
   }
 
-  const confirmTrade = () => {
-    backendApi.post("/trades", {
-      trade: {
-        pokemons_group_one: trainerOne.map(value => value.name),
-        pokemons_group_two: trainerTwo.map(value => value.name),
-        is_fair: isFair
-      }
-    })
-  }
-
   useEffect(() => {
     loadMorePokemons()
   }, [])
 
 
+  const confirmTrade = () => {
+    if (trainerOne.length && trainerTwo.length) {
+      backendApi.post("/trades", {
+        trade: {
+          pokemons_group_one: trainerOne.map(value => value.name),
+          pokemons_group_two: trainerTwo.map(value => value.name),
+          is_fair: isFair
+        }
+      }).then(function () {
+        setResultMessage("Trade registrada com sucesso !");
+      })
+    } else {
+      setResultMessage("Deve haver no minimo um pokemon de cada lado");
+    }
+  }
+
   useEffect(() => {
 
-    if (trainerOne.length != 0 && trainerTwo.length != 0) {
+    if (trainerOne.length && trainerTwo.length) {
       let trainerOneXP = trainerOne.map(item => item.base_experience).reduce((prev, next) => prev + next);
       let trainerTwoXP = trainerTwo.map(item => item.base_experience).reduce((prev, next) => prev + next);
       let totalXPs = [trainerOneXP, trainerTwoXP].sort(function (a, b) { return a - b })
@@ -73,67 +81,22 @@ function Trade() {
 
 
   return (
-    <div>
-      <div className="container ">
+    <div className="container ">
 
-        {!!(trainerOne.length && trainerTwo.length) &&
-          <Message type={isFair ? 'success' : 'danger'} content={isFair ? 'A troca está justa !' 
+      {!!(trainerOne.length && trainerTwo.length) &&
+        <Message type={isFair ? 'success' : 'danger'} content={isFair ? 'A troca está justa !'
           : `A troca não está justa, são necessários ${missingXp.toFixed(2)} de XP para balancear`} />
-        }
+      }
 
-        <div className="d-flex justify-content-center">
+      {!!(resultMessage) &&
+        <Message type="dark" content={resultMessage} />
+      }
 
-          <button className="btn btn-dark m-2" onClick={() => confirmTrade()}>Realizar Troca</button>
-        </div>
-        <div className="row">
-
-          <div className="trainer col">
-            <h6>Treinador 1</h6>
-            {trainerOne.map((pokemon, index) => (
-              <img key={index} src={pokemon.sprite} alt="" />
-            ))}
-          </div>
-          <div className="trainer col">
-            <h6>Treinador 2</h6>
-            {trainerTwo.map((pokemon, index) => (
-              <img key={index} src={pokemon.sprite} alt="" />
-            ))}
-          </div>
-        </div>
-        <div className="sprites_row">
-
-          <div className="infinite-scroll">
-
-            <InfiniteScroll
-              pageStart={0}
-              loadMore={loadMorePokemons}
-              hasMore={hasMore}
-              loader={<div className="loader" key={0}>Loading ...</div>}
-            >
-              <div className="pokemon-list">
-                {pokemons.map((pokemon, index) => (
-                  <div className="pokemon">
-                    <div className="card" >
-                      <img className="card-img-top" src={pokemon.sprite} alt="Card image cap" />
-                      <div className="card-body">
-                        <h6 className="card-text">{pokemon.name}</h6>
-                        <button className="btn btn-primary m-1"
-                          onClick={() => addTrainerPokemons(pokemon, "trainerOne")}>T1</button>
-                        <button className="btn btn-secondary m-1"
-                          onClick={() => addTrainerPokemons(pokemon, "trainerTwo")}
-                        >T2</button>
-
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </InfiniteScroll>
-          </div>
-
-        </div>
+      <div className="d-flex justify-content-center">
+        <button className="btn btn-dark m-2" onClick={() => confirmTrade()}>Realizar Troca</button>
       </div>
-
+      <TrainerContainer trainerOne={trainerOne} trainerTwo={trainerTwo} />
+      <PokemonsContainer loadMorePokemons={loadMorePokemons} pokemons={pokemons} addTrainerPokemons={addTrainerPokemons} />
     </div>
   )
 }
